d3fce32a77dcf9c1459594634be73a6b8d2ae86f
Kirill <qbx_program@mail.ru>
2021-05-13 12:19:29 +0300

    Решение задачи 2 до урока
    1 способ

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 #ДЗ2
 
-#ДЗ1 Выяснить тип
+#Задание 1 Выяснить тип
 
 print(type(15 * 3))
 print(type(15 / 3))
 print(type(15 // 2))
 print(type(15 ** 2))
+
+#Задание 2 Работа со списком
+
+text = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+
+index_list = 0
+for i in text:
+    if i.isdigit():
+        if len(i) < 2:
+            text[index_list] = '"0' + i +'"'
+        else:
+            text[index_list] = '"' + i + '"'
+    index_list += 1
+
+print(" ".join(text))
